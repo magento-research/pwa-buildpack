fad9f8a78d0c61df0565893723ecf019eb6a1e9c
Adrien Louis-Rossignol <adrien@occitech.fr>
2018-03-17 23:51:50 +0100

    Update MagentoRootComponentsPlugin to the new Webpack 4 Tapable hooks

--- src/MagentoRootComponentsPlugin/index.js
+++ src/MagentoRootComponentsPlugin/index.js
@@ -38,49 +38,55 @@ class MagentoRootComponentsPlugin {
         );
 
         const moduleByPath = new Map();
-        compiler.plugin('compilation', compilation => {
-            compilation.plugin('normal-module-loader', (loaderContext, mod) => {
-                if (seenRootComponents.has(mod.resource)) {
-                    // The module ("mod") has not been assigned an ID yet,
-                    // so we need to keep a reference to it which will allow
-                    // us to grab the ID during the emit phase
-                    moduleByPath.set(mod.resource, mod);
-                }
-                // To create a unique chunk for each RootComponent, we want to inject
-                // a dynamic import() for each RootComponent, within each entry point.
-                const isAnEntry = compilation.entries.some(entryMod => {
-                    // Check if the module being constructed matches a defined entry point
-                    if (mod === entryMod) return true;
-                    if (!entryMod.identifier().startsWith('multi')) {
-                        return false;
+        compiler.hooks.compilation.tap(
+            'MagentoRootComponentsPlugin',
+            compilation => {
+                compilation.hooks.normalModuleLoader.tap(
+                    'MagentoRootComponentsPlugin',
+                    (loaderContext, mod) => {
+                        if (seenRootComponents.has(mod.resource)) {
+                            // The module ("mod") has not been assigned an ID yet,
+                            // so we need to keep a reference to it which will allow
+                            // us to grab the ID during the emit phase
+                            moduleByPath.set(mod.resource, mod);
+                        }
+                        // To create a unique chunk for each RootComponent, we want to inject
+                        // a dynamic import() for each RootComponent, within each entry point.
+
+                        const isAnEntry = compilation.entries.some(entryMod => {
+                            // Check if the module being constructed matches a defined entry point
+                            if (mod === entryMod) return true;
+                            if (!entryMod.identifier().startsWith('multi')) {
+                                return false;
+                            }
+
+                            // If a multi-module entry is used (webpack-dev-server creates one), we
+                            // need to try and match against each dependency in the multi module
+                            return entryMod.dependencies.some(
+                                singleDep => singleDep.module === mod
+                            );
+                        });
+                        if (!isAnEntry) return;
+
+                        // If this module is an entry module, inject a loader in the pipeline
+                        // that will force creation of all our RootComponent chunks
+                        mod.loaders.push({
+                            loader: loaderPath,
+                            options: {
+                                rootsDirs: rootComponentsDirsAbs
+                            }
+                        });
                     }
+                );
+            }
+        );
 
-                    // If a multi-module entry is used (webpack-dev-server creates one), we
-                    // need to try and match against each dependency in the multi module
-                    return entryMod.dependencies.some(
-                        singleDep => singleDep.module === mod
-                    );
-                });
-                if (!isAnEntry) return;
-
-                // If this module is an entry module, inject a loader in the pipeline
-                // that will force creation of all our RootComponent chunks
-                mod.loaders.push({
-                    loader: loaderPath,
-                    options: {
-                        rootsDirs: rootComponentsDirsAbs
-                    }
-                });
-            });
-        });
-
-        compiler.plugin('emit', (compilation, cb) => {
+        compiler.hooks.emit.tap('MagentoRootComponentsPlugin', compilation => {
             // Prepare the manifest that the Magento backend can use
             // to pick root components for a page.
-            const namedChunks = Array.from(
-                Object.values(compilation.namedChunks)
-            );
-            const manifest = namedChunks.reduce((acc, chunk) => {
+            const chunks = Array.from(compilation.chunks);
+
+            const manifest = chunks.reduce((acc, chunk) => {
                 const { rootDirective, rootComponentPath } =
                     rootComponentMap.get(chunk.name) || {};
                 if (!rootDirective) return acc;
@@ -102,7 +108,6 @@ class MagentoRootComponentsPlugin {
             compilation.assets[this.manifestFileName] = new RawSource(
                 JSON.stringify(manifest, null, 4)
             );
-            cb();
         });
     }
 }
