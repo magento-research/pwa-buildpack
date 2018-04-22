51fd50d738cdec5ab914a0d51d4b86374aef6a26
zetlen <zetlen@gmail.com>
2018-04-22 11:24:53 -0700

    style: additional comment

--- src/WebpackTools/middlewares/OriginSubstitution.js
+++ src/WebpackTools/middlewares/OriginSubstitution.js
@@ -6,6 +6,9 @@
  * Rather than configure Magento to use the your temporary dev server URL as
  * its configured base domain, this middleware allows the dev server to text
  * replace any links, resources, or reference URLs on the fly.
+ *
+ * For Magento 2 specifically, This is a stopgap until we can hack Framework to
+ * have branch logic in asset URL resolvers.
  */
 const debug = require('../../util/debug').makeFileLogger(__filename);
 const url = require('url');
